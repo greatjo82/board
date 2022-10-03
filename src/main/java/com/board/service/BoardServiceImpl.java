914f17c9b4b5c903ba8024326708455054f7bd5a
bluelow <c292513@hanmail.net>
2022-10-03 16:30:27 +0900

    1. business tier 구현완료(boardService 인터페이스 + boardServiceImpl 클래스) 2. presentation tier 구현완료(Controller 클래스)

---
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -0,0 +1,60 @@
+package com.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.domain.BoardVO;
+import com.board.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+@AllArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		log.info("resister...." + board);
+		mapper.insertSelectKey(board);
+		
+	}
+	
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get....." + bno);
+		
+		return mapper.read(bno);
+	}
+	
+	@Override
+	public boolean modify(BoardVO board) {
+		
+		log.info("modify...." + board);
+		
+		return mapper.update(board) == 1;
+	}
+	
+	@Override
+	public boolean remove(Long bno) {
+		log.info("remove...." + bno);
+		
+		return mapper.delete(bno) == 1;
+	}
+	
+	@Override
+	public List<BoardVO> getList() {
+		log.info("getList....");
+		
+		return mapper.getList();
+	}
+	
+}
