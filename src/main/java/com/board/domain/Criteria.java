03c8e952f9079993298344263fc098691cab366f
bluelow <c292513@hanmail.net>
2022-10-15 21:26:55 +0900

    검색기능 추가

--- src/main/java/com/board/domain/Criteria.java
+++ src/main/java/com/board/domain/Criteria.java
@@ -12,6 +12,9 @@ public class Criteria {
 	private int pageNum; 	// 페이지 수
 	private int amount;		// 페이지당 보여줄 데이터
 
+	private String type;	//검색 타입(글쓴이(W)/제목(T)/내용(C)) 
+	private String keyword; //검색어
+	
 	public Criteria() {
 		this(1,10);		//초기값 : 한 페이지 당 10개의 데이터 리스트를 보여준다.
 	}
@@ -20,4 +23,11 @@ public class Criteria {
 		this.pageNum = pageNum;
 		this.amount = amount;
 	}
+	
+	public String[] getTypeArr(){
+		
+		//변수 type에 값이 없으면 새로운 문자열 object 생성, 값이 있다(T/W/C)면 공백("") 기준으로 각 쪼갬(ex. TC -> T, C  TW -> T,W , TCW -> T,C,W) 
+		return type == null? new String[]{} : type.split("");
+	}
+	
 }
--- src/test/java/com/board/mapper/BoardMapperTests.java
+++ src/test/java/com/board/mapper/BoardMapperTests.java
@@ -30,7 +30,7 @@ public class BoardMapperTests {
 	
 	}
 	
-	@Test
+//	@Test
 	public void testPaging(){
 		Criteria cri = new Criteria();
 	
@@ -43,6 +43,18 @@ public class BoardMapperTests {
 		list.forEach(board -> log.info(board));
 		
 	}
+	
+	@Test
+	public void testSearch(){
+	
+		Criteria cri = new Criteria();
+		cri.setKeyword("새로");
+		cri.setType("TC");
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
+	
 	
 //	@Test
 	public void testInsertSelectKey(){
